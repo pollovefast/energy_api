e586119d1f0edfb929d6d772cc22f819e7096d66
patinyas62 <patinyas62@email.nu.ac.th>
2020-12-02 20:09:23 +0700

    wwww

--- server.js
+++ server.js
@@ -296,7 +296,7 @@ app.get('/alldata', (req,res) => {
         File.find({}, {}, { sort: { 'create': -1 } }, function (err, result) {
             res.status(200).send(result)
             console.log('show data')
-        }).limit(5).catch(err => {
+        }).catch(err => {
             res.status(400).send({
                 msg: "no data or type not support"
             })
